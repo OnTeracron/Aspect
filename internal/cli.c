6f6a3153c015beb7e3a5119b4794356ca644f55d
Savion Goslee <saviongoslee@gmail.com>
2024-05-03 14:37:25 -0700

    Add interactive terminal section

--- internal/cli.c
+++ internal/cli.c
@@ -29,7 +29,8 @@ void AspectCLI_Main(int argc, char** argv) {
     char* filename = AspectCLI_is_flag_set(argc, argv, "-f", 1);
 
     if (strcmp(filename, "FLAG_NOT_SET") == 0) { 
-        AspectCLI_exiterror("MissingFileError", "An existing flag must be set for this command.");
+        /* Interactive Terminal */
+        exit(EXIT_SUCCESS);
     } else if (strcmp(filename, "ARGUMENT_NOT_GIVEN") == 0) {
         AspectCLI_exiterror("FlagError", "Filename expected for the -f flag.\nUsage: aspect -f [filename]");
     }
